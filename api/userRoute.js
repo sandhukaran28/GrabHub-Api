9a47699dde086b6e706d54e4d1b39b7648d204ef
sandhukaran28 <60167401+sandhukaran28@users.noreply.github.com>
2021-11-21 08:24:38 +0530

    login & logout route added

--- api/userRoute.js
+++ api/userRoute.js
@@ -54,17 +54,78 @@ router.post('/auth/new', async (req, res) => {
 
         const savedUser = await newUser.save();
 
+        // sign the token
         const token = jwt.sign({
             user: savedUser._id
         }, process.env.JWT_SECRET_KEY);
-        console.log(token);
-        res.status(200).json({
-            successMessage: 'Account Added',
-        });
+
+        // sending token
+        res.cookie("token", token, {
+            httpOnly: true,
+        }).send();
+
     } catch (e) {
         console.error(e);
         res.sendStatus(500);
     }
+})
+
+
+router.post('/auth/login', async (req, res) => {
+
+    try {
+
+        const {
+            email,
+            password,
+        } = req.body;
+
+        if (!email || !password) {
+
+            res.status(400).json({
+                errorMessage: 'Please Enter all required fields'
+            });
+        }
+        const existingUser = await User.findOne({
+            email
+        });
+
+        if (!existingUser) {
+            res.status(401).json({
+                errorMessage: 'Wrong Email or password'
+            });
+        }
+
+        const passwordCorrect = await bcrypt.compare(password, existingUser.passwordHash);
+
+        if (!passwordCorrect) {
+            res.status(401).json({
+                errorMessage: 'Wrong Email or password'
+            });
+        }
+
+        // sign the token
+        const token = jwt.sign({
+            user: existingUser._id
+        }, process.env.JWT_SECRET_KEY);
+
+        // sending token
+        res.cookie("token", token, {
+            httpOnly: true,
+        }).send();
+
+    } catch (e) {
+        console.error(e);
+        res.sendStatus(500);
+    }
+})
+
+router.get('/auth/logout', (req, res) => {
+
+    res.cookie("token", "", {
+        httpOnly: true,
+        expires: new Date(0)
+    }).send();
 })
 
 
